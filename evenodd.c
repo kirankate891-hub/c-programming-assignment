5c2101dbc004dc2936b98193577d6ea063cd224d
kiran kate <kirankate891@gmail.com>
2026-08-09 19:39:22 +0530

    Refactor evenodd.c to remove duplicate code

--- evenodd.c
+++ evenodd.c
@@ -6,19 +6,7 @@ if(n%2==0)
 printf("even");
 else
 printf("odd");
-return 0;}
-
-
-
-#include<stdio.h>
-int main()
-{
-	int n=0;
-	if(n%2==0)
-	printf("even");
-	else
-	printf("odd");
-	return 0;
+return 0;
 }
 
 
