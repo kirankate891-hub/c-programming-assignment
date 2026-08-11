c4ee00342ee09b94111d86a401f3393a8ee7ff56
kiran kate <kirankate891@gmail.com>
2026-08-11 22:58:55 +0530

    Add program to check if a year is a leap year

---
+++ Assignment1/program6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter year:");
+	scanf("%d",&year);
+	
+	if((year % 400==0)||(year%4==0 && year % 100!=0))
+	printf("Leaf year");
+	else
+	printf("Not a leaf year");
+	return 0;
+}
+
+
