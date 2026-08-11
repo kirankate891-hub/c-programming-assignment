413bb3cbf838c6b75774ef7014cc80faf7251403
kiran kate <kirankate891@gmail.com>
2026-08-11 22:57:21 +0530

    Add program4.c to check if a number is even or odd

---
+++ Assignment1/program4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n=10;
+if(n%2==0)
+printf("even");
+else
+printf("odd");
+return 0;}
+
+
+
+#include<stdio.h>
+int main()
+{
+	int n=0;
+	if(n%2==0)
+	printf("even");
+	else
+	printf("odd");
+	return 0;
+}
+
+
+
