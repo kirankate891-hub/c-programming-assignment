14de66e8d979f3868451cbf942fc4edaa60392b5
kiran kate <kirankate891@gmail.com>
2026-08-13 19:51:13 +0530

    Add basic calculator program in C

---
+++ assignment2/program8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    char op;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+
+    printf("Enter operator: ");
+    scanf(" %c", &op);
+
+    if (op == '+')
+        printf("%d", a + b);
+    else if (op == '-')
+        printf("%d", a - b);
+    else if (op == '*')
+        printf("%d", a * b);
+    else if (op == '/')
+        printf("%d", a / b);
+    else if (op == '%')
+        printf("%d", a % b);
+    else
+        printf("Invalid Operator");
+
+    return 0;
+}
